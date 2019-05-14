a6acc3ab1c181e8b959d6316926c1dea82d3a3ce
duhon <duhon@rambler.ru>
2019-05-13 23:57:41 -0500

    remove docker-compose and .env files

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+## LINUX ##
 #requare
 sudo apt update
 sudo apt install htop
@@ -15,12 +16,32 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+## MAC OS ##
+#requare
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#install docker
+brew cask install docker
+#optimization for mac
+#https://gist.github.com/tombigel/d503800a282fcadbee14b537735d202c
+
+## WINDOWS ## PowerShell
+Invoke-WebRequest "https://master.dockerproject.org/windows/x86_64/docker.zip" -OutFile "$env:TEMP\docker.zip" -UseBasicParsing
+Expand-Archive -Path "$env:TEMP\docker.zip" -DestinationPath $env:ProgramFiles
+# Add path to this PowerShell session immediately
+$env:path += ";$env:ProgramFiles\Docker"
+# For persistent use after a reboot
+$Path = [Environment]::GetEnvironmentVariable("Path",[System.EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("Path", $Path + ";$env:ProgramFiles\Docker", [EnvironmentVariableTarget]::Machine)
+
 #prepare data
-sudo mkdir ~/www
-sudo chmod 777 ~/www
-sudo chown -R ubuntu:root .
-mkdir ~/www/.composer
-git clone https://github.com/magento/magento2ce.git /var/www/magento2ce
-git clone https://github.com/magento/magento2ee.git /var/www/magento2ee
-git clone https://github.com/magento/magento2b2b.git /var/www/magento2b2b
-git clone https://github.com/duhon/magento-docker.git magento
+mkdir ~/www
+cd ~/www/
+chown -R $USER:root .
+chmod g+s .
+git clone https://github.com/duhon/magento-docker.git
+cp magento-docker/bundles/typical.yml magento-docker/docker-compose.yml
+cp magento-docker/.env.dist magento-docker/.env
+git clone https://github.com/magento/magento2ce.git
+git clone https://github.com/magento/magento2ee.git
+git clone https://github.com/magento/magento2b2b.git
