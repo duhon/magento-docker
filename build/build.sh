56c3ded193291cc604267b60715f291498c1fbb8
duhon <duhon@rambler.ru>
2019-10-14 17:35:30 -0500

    remove useless data (blackfire)

--- build/build.sh
+++ build/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+set -ex
 type=${1:-'fpm'}
 ver=${2:-'7.2'}
 mage=${3:-'2.3.0'}
 docker build -f ./php/${type} -t duhon/php:${ver}-${type} --compress --force-rm --pull .
-docker run --name test -it duhon/php:${ver}-${type} bash -c 'git clone -b '${mage}' --depth=1 --single-branch https://github.com/magento/magento2.git /var/www/magento2ce && composer install && php -v && php -m && php -d memory_limit=-1 /var/www/magento2ce/vendor/bin/phpunit -c /var/www/magento2ce/dev/tests/unit/phpunit.xml.dist'
+echo "docker push duhon/php:${ver}-${type}"
+docker run --name test -it duhon/php:${ver}-${type} bash -c 'git clone -b '${mage}' --depth=1 --single-branch https://github.com/magento/magento2.git /var/www/magento2ce && composer up && php -v && php -m && php -d memory_limit=-1 /var/www/magento2ce/vendor/bin/phpunit -c /var/www/magento2ce/dev/tests/unit/phpunit.xml.dist'
 docker commit test duhon/php-magento:${mage}-${type}
 docker rm -f test
-echo "docker push duhon/php:${ver}-${type}"
 echo "docker push duhon/php-magento:${mage}-${type}"
 
 #cd ~/www
