36deee191825f154202c1b3d3c71f1eaa3737ccc
duhon <duhon@rambler.ru>
2019-11-01 21:04:40 -0500

    rewrite magento2_debug

--- build/build.sh
+++ build/build.sh
@@ -7,6 +7,8 @@ docker build -f ./php/${type} -t duhon/php:${ver}-${type} --compress --force-rm 
 docker run --rm --name test -it duhon/php:${ver}-${type} bash -c 'git clone -b '${mage}' --depth=1 --single-branch https://github.com/magento/magento2.git /var/www/magento2ce && composer update && php -v && php -m && php -d memory_limit=-1 /var/www/magento2ce/vendor/bin/phpunit -c /var/www/magento2ce/dev/tests/unit/phpunit.xml.dist'
 echo "docker push duhon/php:${ver}-${type}"
 
+#todo: check hash for files nginx.conf.sample, php/tests/*
+
 #cd ~/www
 #shopt -s dotglob
 #zip -r0 ../data.zip *
