5d4611b368d36571905bf460b316c502a8496238
Michail Slabko <mslabko@magento.com>
2021-02-08 19:29:31 -0600

    SFAPP-244: Ability to install storefront microservices standalone

--- reinstall.sh
+++ reinstall.sh
@@ -30,13 +30,13 @@ fi
 
 
 # monolith installation with Commerce Data export
-docker-compose exec app magento prepare_monolith_installation
-docker-compose exec app magento reinstall_monolith ${INSTALLED_REPOS}
-docker-compose exec app magento config_setup
-docker-compose exec app magento storefront
+docker-compose -f bundles/monolith.yml exec app magento prepare_monolith_installation
+docker-compose -f bundles/monolith.yml exec app magento reinstall_monolith ${INSTALLED_REPOS}
+docker-compose -f bundles/monolith.yml exec app magento config_setup
+docker-compose -f bundles/monolith.yml exec app magento storefront
 
 if [ ${MONOLITHIC_INSTALLATION} == "YES" ]; then
-   docker-compose exec app magento grpc &
+   docker-compose -f bundles/monolith.yml exec app magento grpc &
 fi
 
 # standalone installations
@@ -50,29 +50,28 @@ if [ ${MONOLITHIC_INSTALLATION} == "NO" ]; then
 
 
     if [[ $INSTALLED_REPOS == *"storefront-message-broker"* ]]; then
-        docker-compose exec app-message-broker magento reinstall_message_broker
-        docker-compose exec app-message-broker magento grpc &
-
+        docker-compose -f bundles/message-broker.yml -f bundles/monolith.yml exec app-message-broker magento reinstall_message_broker
     fi
 
     if [[ $INSTALLED_REPOS == *"catalog-storefront"* ]]; then
-        docker-compose exec app-catalog-storefront magento reinstall_catalog_storefront
+        docker-compose -f bundles/catalog.yml -f bundles/monolith.yml exec app-catalog-storefront magento reinstall_catalog_storefront
         # start gRPC server on standalone application
-        docker-compose exec app-catalog-storefront magento grpc &
+        docker-compose -f bundles/catalog.yml -f bundles/monolith.yml exec app-catalog-storefront magento grpc &
     fi
 
     if [[ $INSTALLED_REPOS == *"storefront-product-reviews"* ]]; then
-        docker-compose exec app-product-reviews magento reinstall_storefront_review
+        docker-compose -f bundles/product-review.yml -f bundles/monolith.yml exec app-product-reviews magento reinstall_storefront_review
+        docker-compose -f bundles/product-review.yml -f bundles/monolith.yml exec app-product-reviews magento grpc &
     fi
 
     if [[ $INSTALLED_REPOS == *"storefront-pricing-ce"* ]]; then
-        docker-compose exec app-pricing magento reinstall_storefront_pricing
-        docker-compose exec app-pricing magento grpc &
+        docker-compose -f bundles/pricing.yml -f bundles/monolith.yml exec app-pricing magento reinstall_storefront_pricing
+        docker-compose -f bundles/pricing.yml -f bundles/monolith.yml exec app-pricing magento grpc &
     fi
 
     if [[ $INSTALLED_REPOS == *"storefront-search-ce"* ]]; then
-        docker-compose exec app-search magento reinstall_storefront_search
-        docker-compose exec app-search magento grpc &
+        docker-compose -f bundles/search.yml -f bundles/monolith.yml exec app-search magento reinstall_storefront_search
+        docker-compose -f bundles/search.yml -f bundles/monolith.yml exec app-search magento grpc &
     fi
 fi
 
