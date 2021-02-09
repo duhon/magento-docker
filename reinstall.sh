a54b0379ef90c1861062415d65804c620097d224
Michail Slabko <mslabko@magento.com>
2021-02-08 20:56:37 -0600

    SFAPP-244: Ability to install storefront microservices standalone

--- reinstall.sh
+++ reinstall.sh
@@ -50,28 +50,28 @@ if [ ${MONOLITHIC_INSTALLATION} == "NO" ]; then
 
 
     if [[ $INSTALLED_REPOS == *"storefront-message-broker"* ]]; then
-        docker-compose -f bundles/message-broker.yml -f bundles/monolith.yml exec app-message-broker magento reinstall_message_broker
+        docker-compose -f bundles/message-broker.yml -f bundles/monolith.yml exec message-broker magento reinstall_message_broker
     fi
 
     if [[ $INSTALLED_REPOS == *"catalog-storefront"* ]]; then
-        docker-compose -f bundles/catalog.yml -f bundles/monolith.yml exec app-catalog-storefront magento reinstall_catalog_storefront
+        docker-compose -f bundles/catalog.yml -f bundles/monolith.yml exec catalog magento reinstall_catalog_storefront
         # start gRPC server on standalone application
-        docker-compose -f bundles/catalog.yml -f bundles/monolith.yml exec app-catalog-storefront magento grpc &
+        docker-compose -f bundles/catalog.yml -f bundles/monolith.yml exec catalog magento grpc &
     fi
 
     if [[ $INSTALLED_REPOS == *"storefront-product-reviews"* ]]; then
-        docker-compose -f bundles/product-review.yml -f bundles/monolith.yml exec app-product-reviews magento reinstall_storefront_review
-        docker-compose -f bundles/product-review.yml -f bundles/monolith.yml exec app-product-reviews magento grpc &
+        docker-compose -f bundles/product-review.yml -f bundles/monolith.yml exec product-reviews magento reinstall_storefront_review
+        docker-compose -f bundles/product-review.yml -f bundles/monolith.yml exec product-reviews magento grpc &
     fi
 
     if [[ $INSTALLED_REPOS == *"storefront-pricing-ce"* ]]; then
-        docker-compose -f bundles/pricing.yml -f bundles/monolith.yml exec app-pricing magento reinstall_storefront_pricing
-        docker-compose -f bundles/pricing.yml -f bundles/monolith.yml exec app-pricing magento grpc &
+        docker-compose -f bundles/pricing.yml -f bundles/monolith.yml exec pricing magento reinstall_storefront_pricing
+        docker-compose -f bundles/pricing.yml -f bundles/monolith.yml exec pricing magento grpc &
     fi
 
     if [[ $INSTALLED_REPOS == *"storefront-search-ce"* ]]; then
-        docker-compose -f bundles/search.yml -f bundles/monolith.yml exec app-search magento reinstall_storefront_search
-        docker-compose -f bundles/search.yml -f bundles/monolith.yml exec app-search magento grpc &
+        docker-compose -f bundles/search.yml -f bundles/monolith.yml exec search magento reinstall_storefront_search
+        docker-compose -f bundles/search.yml -f bundles/monolith.yml exec search magento grpc &
     fi
 fi
 
