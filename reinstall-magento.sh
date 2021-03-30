18614674c2c6900229c560175424d8ad19b1199d
Michail Slabko <mslabko@magento.com>
2021-03-30 14:41:34 -0500

    reinstall magento

--- reinstall-magento.sh
+++ reinstall-magento.sh
@@ -17,8 +17,5 @@ cd $DOCKER_PROJECT_DIR
 
 
 # composer based installation
-docker-compose -f bundles/monolith.yml exec app magento reinstall_monolith ${INSTALLED_REPOS}
-docker-compose -f bundles/monolith.yml exec app magento config_setup
-docker-compose -f bundles/monolith.yml exec app bin/magento -f setup:static-content:deploy
-docker-compose -f bundles/monolith.yml exec app bin/magento module:disable Magento_TwoFactorAuth
-docker-compose -f bundles/monolith.yml exec app rm -rf generated/
+docker-compose exec app magento reinstall_monolith
+docker-compose exec app magento config_setup
